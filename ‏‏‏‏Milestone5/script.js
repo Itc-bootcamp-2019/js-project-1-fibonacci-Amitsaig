e39367e2a413c8414a71330a74f87d135e07a3d2
Amitsaig <amitsa1995@gmail.com>
2020-03-03 14:51:06 +0200

    update

--- ‏‏‏‏Milestone5/script.js
+++ ‏‏‏‏Milestone5/script.js
@@ -1,4 +1,11 @@
 function local(number) {
 	http://localhost:5050/fibonacci/:number
 }
-
+fetch
+/*function changer() {
+	let inputnum = document.getElementById('func').value;
+	let y = fibo(inputnum);
+	document.getElementById('fibi').innerText = y;
+}
+document.getElementById("myBtn").addEventListener("click",changer);
+*/
